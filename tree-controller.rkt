9f23b3016278020c3618d3ae26ba6601433c89c3
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-06 04:28:05 +0800

    more more example

--- tree-controller.rkt
+++ tree-controller.rkt
@@ -1,19 +1,42 @@
 #lang racket/base
-(require ffi/unsafe/objc)
+(require ffi/unsafe
+         ffi/unsafe/objc
+         ffi/unsafe/nsstring
+         "utils.rkt")
 
-(import-class NSSplitViewController
+(import-class NSObject
+              NSString
+              NSRect
+              NSView
+              NSSplitViewController
               NSSplitView
               NSSplitViewItem
               NSOutlineView
               NSTreeController
               )
 
-(with-autorelease
-    (define treeView (tell NSOutlineView alloc))
-  (define editorView (tell NSOutlineView alloc))
-  (define mainView (tell NSSplitView alloc))
-  (define mainViewC (tell NSSplitViewController alloc))
-  (tellv mainViewC addSplitViewItem: treeView)
-  (tellv mainViewC addSplitViewItem: editorView)
+(define-cocoa NSLog (_fun _NSString -> _void))
 
-  )
+(define-objc-class Photo NSObject
+  (cap pho)
+  (- _NSString (caption) cap)
+  (- _NSString (photographer) pho)
+  (- _void (setCaption: [_NSString input])
+     (set! cap input))
+  (- _void (setPhotographer: [_NSString input])
+     (set! pho input))
+  (- _void (dealloc)
+     (tell cap release)
+     (tell pho release)
+     (super-tell dealloc)))
+
+(define p (tell (tell Photo alloc) init))
+(define pho (tell (tell NSString alloc)
+                  initWithUTF8String: #:type _string "photo"))
+(tell p setPhotographer: pho)
+(NSLog "hkle")
+(tell p dealloc)
+
+(define mainView (tell (tell NSSplitView alloc) init))
+(define mainViewC (tell NSSplitViewController alloc))
+mainView
