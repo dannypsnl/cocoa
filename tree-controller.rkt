31c3a166bb2357c3c8dccee1f10e3fa3dec0aeb9
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-09 15:56:46 +0800

    only outlineview, and title style

--- tree-controller.rkt
+++ tree-controller.rkt
@@ -8,9 +8,6 @@
               NSApplication
               NSRect
               NSView
-              NSSplitViewController
-              NSSplitView
-              NSSplitViewItem
               NSOutlineView
               NSTreeController
               )
@@ -18,13 +15,13 @@
 (call-with-autorelease
  (lambda ()
    (define frame (make-NSRect (make-NSPoint 0 0) (make-NSSize 200 200)))
-   (define mainView (tell (tell NSSplitView alloc)
+   (define mainView (tell (tell NSOutlineView alloc)
                           initWithFrame: #:type _NSRect frame))
 
    (define app (tell NSApplication sharedApplication))
    (define win (tell (tell NSWindow alloc)
                      initWithContentRect: #:type _NSRect frame
-                     styleMask: #:type _int 0
+                     styleMask: #:type _int 1
                      backing: #:type _int 2
                      defer: NO))
    (tell win setTitle: #:type _NSString "editor")
